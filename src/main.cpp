b92568f6482127b185254e90fec00923cb1cb5fd
Lateralus138 <faithnomoread@yahoo.com>
2022-03-10 21:18:01 -0600

    Another build test - Thu, Mar 10, 2022  9:18:01 PM

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
+#include <cmath>
 // #include <string>
 // #include <filesystem>
 int main()
 {
   std::string ln = u8"\U0001F916";
+
+  const std::string ROBOT = u8"\U0001F916";
+  std::string line;
+  std::string message = " Welcome to C++ in Android! ";
+  for (auto i = 0; i < ((int) floor(ln.length() / 4) + 2); i++) line.append(ROBOT);
   std::cout
-    << "The length of ln: [" << ln << "] is [" << ln.length()
+    << line
+    << "`nThe length of ln: [" << ln << "] is [" << ln.length()
     << "]\nand\nThe size of ln: [" << ln << "] is [" << ln.size() << "]\n";
   // const std::string ROBOT = u8"\U0001F916";
   // std::string line;
