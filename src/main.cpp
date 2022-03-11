901e24dd6d878b22be410cfa51967b45b6259551
Lateralus138 <faithnomoread@yahoo.com>
2022-03-10 19:26:22 -0600

    Another build test - Thu, Mar 10, 2022  7:26:22 PM

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,6 @@ int main()
 {
   const std::string ROBOT = u8"\U0001F916";
   std::string line;
-  for (auto i = 0; i < 30; i++) line.append(ROBOT);
+  for (auto i = 0; i < 15; i++) line.append(ROBOT);
   std::cout << line << u8"\n" << ROBOT << " Welcome to C++ in Android! " << ROBOT << "\n" << line << '\n';
 }
