4e88734e0167025aa60d72716f3226ae1e86d589
Lateralus138 <faithnomoread@yahoo.com>
2022-03-10 19:19:35 -0600

    Another build test - Thu, Mar 10, 2022  7:19:35 PM

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <string>
-std::string line(50, u8'\U0001F916');
 int main()
 {
-  std::cout << line << u8"\n \U0001F916 Welcome to C++ in Android! \U0001F916 \n" << line << '\n';
+  const std::string ROBOT = u8"\U0001F916";
+  std::string line;
+  for (auto i = 0; i < 50; i++) line.append(ROBOT);
+  std::cout << line << u8"\n " << ROBOT << " Welcome to C++ in Android! " << ROBOT << " \n" << line << '\n';
 }
