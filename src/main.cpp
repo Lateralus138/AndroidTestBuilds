eb45e80a43004403bc07f21a20152881701d2b2b
Lateralus138 <faithnomoread@yahoo.com>
2022-03-10 18:59:15 -0600

    Another build test - Thu, Mar 10, 2022  6:59:15 PM

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
 int main()
 {
-  std::cout << u8" \u0001F600 Welcome to C++ in Android! \u0001F600 \n";
+  std::cout << u8" \U0001F600 Welcome to C++ in Android! \U0001F600 \n";
 }
