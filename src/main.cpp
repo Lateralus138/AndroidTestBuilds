e663e46a5ca13ee179d8572b85b3715d0682e26a
Lateralus138 <faithnomoread@yahoo.com>
2022-03-10 21:38:55 -0600

    Another build test - Thu, Mar 10, 2022  9:38:55 PM

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
 #include <cmath>
-// #include <string>
-// #include <filesystem>
-int main()
+int main(int argc, char * argv [])
 {
-  const std::string ROBOT = u8"\U0001F916";
+  std::string message;
+  if (argc > 1)
+  {
+    for (auto i = 1; i < argc; i++)
+    {
+      if (i > 1) message.push_back(' ');
+      message.append(argv[i]);
+    }
+  }
+  else
+  {
+    std::cerr << "Not enough arguments passed to the program.\n";
+    return 1;
+  }
+  const std::string ROBOT = u8"\U0001F480";
   std::string line;
-  std::string message = " Welcome to C++ in Android! ";
   for (auto i = 0; i < ((int) floor(message.length() / 2) + 2); i++) line.append(ROBOT);
   std::cout << line << '\n' << ROBOT << message << ROBOT << '\n' << line << '\n';
-  // const std::string ROBOT = u8"\U0001F916";
-  // std::string line;
-  // for (auto i = 0; i < 15; i++) line.append(ROBOT);
-  // line.append(ROBOT);
-  // std::cout << line << u8"\n" << ROBOT << " Welcome to C++ in Android! " << ROBOT << "\n" << line << '\n';
 }
