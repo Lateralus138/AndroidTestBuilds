3d9b7c3327b7b5e1e54c43affb0bf8e84fc8dc61
Lateralus138 <faithnomoread@yahoo.com>
2022-03-18 23:00:54 -0500

    New build - Fri Mar 18 23:00:54 CDT 2022

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 SHELL = bash
-PROJNM = works
+PROJNM = andronixlxqti
 AND32GXX = arm-linux-gnueabihf-g++
 AND64GXX = aarch64-linux-gnu-g++
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,2 +1,120 @@
 #include <iostream>
-int main() { std::cout << "This fucking works!\n"; } 
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+#include <sys/wait.h>
+int main()
+{
+  std::cout << "\x1b]0;Andronix LXQT Installer\007" << std::flush;
+  const std::string SPACER(50, '-');
+  char
+    * argsUpdate[] = {(char *)"pkg", (char *)"update", (char *)"-y", NULL},
+    * argsInstall[] =
+    {
+      (char *)"pkg",
+      (char *)"install",
+      (char *)"wget",
+      (char *)"curl",
+      (char *)"proot",
+      (char *)"tar",
+      (char *)"-y",
+      NULL
+    },
+    * argsWget[] =
+    {
+      (char *)"wget",
+      (char *)
+        "https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/"
+        "master/Installer/Ubuntu19/ubuntu19-lxqt.sh", NULL
+    },
+    * argsChmod[] = {(char *)"chmod", (char *)"+x", (char *)"ubuntu19-lxqt.sh", NULL},
+    * argsRun[] = {(char *)"bash", (char *)"ubuntu19-lxqt.sh", NULL};
+  int sysResult = 0;
+  pid_t pid;
+  std::cout <<  SPACER << "\nAttempting to run 'pkg update -y'\n";
+  pid = fork();
+  if (pid > -1)
+  {
+    if (pid == 0) sysResult = execvp(argsUpdate[0], argsUpdate);
+    else wait(nullptr);
+  }
+  else
+  {
+    std::cerr << "Program failed to fork.\n";
+    return EXIT_FAILURE;
+  }
+  if (sysResult > 0)
+  {
+    std::cerr << "Error in 'pkg update -y'.\n";
+    return 1;
+  }
+  std::cout << "Successfully ran 'pkg update -y'\n" << SPACER << "\npkg install wget curl proot tar -y'\n";
+  pid = fork();
+  if (pid > -1)
+  {
+    if (pid == 0) sysResult = execvp(argsInstall[0], argsInstall);
+    else wait(nullptr);
+  }
+  else
+  {
+    std::cerr << "Program failed to fork.\n";
+    return EXIT_FAILURE;
+  }
+  if (sysResult > 0)
+  {
+    std::cerr << "Error in 'pkg install wget curl proot tar -y'.\n";
+    return 2;
+  }
+  std::cout << "Successfully ran 'pkg install wget curl proot tar -y'\n" << SPACER << "\nwget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Ubuntu19/ubuntu19-lxqt.sh'\n";
+  pid = fork();
+  if (pid > -1)
+  {
+    if (pid == 0) sysResult = execvp(argsWget[0], argsWget);
+    else wait(nullptr);
+  }
+  else
+  {
+    std::cerr << "Program failed to fork.\n";
+    return EXIT_FAILURE;
+  }
+  if (sysResult > 0)
+  {
+    std::cerr << "Error in 'wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Ubuntu19/ubuntu19-lxqt.sh'.\n";
+    return 3;
+  }
+  std::cout << "Successfully ran 'wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Ubuntu19/ubuntu19-lxqt.sh'\n" << SPACER << "\nAttempting to run 'chmod +x ubuntu19-lxqt.sh'\n";
+  pid = fork();
+  if (pid > -1)
+  {
+    if (pid == 0) sysResult = execvp(argsChmod[0], argsChmod);
+    else wait(nullptr);
+  }
+  else
+  {
+    std::cerr << "Program failed to fork.\n";
+    return EXIT_FAILURE;
+  }
+  if (sysResult > 0)
+  {
+    std::cerr << "Error in 'chmod +x ubuntu19-lxqt.sh'.\n";
+    return 4;
+  }
+  std::cout << "Successfully ran 'chmod +x ubuntu19-lxqt.sh'\n" << SPACER << "\nAttempting to run 'bash ubuntu19-lxqt.sh'\n";
+  pid = fork();
+  if (pid > -1)
+  {
+    if (pid == 0) sysResult = execvp(argsRun[0], argsRun);
+    else wait(nullptr);
+  }
+  else
+  {
+    std::cerr << "Program failed to fork.\n";
+    return EXIT_FAILURE;
+  }
+  if (sysResult > 0)
+  {
+    std::cerr << "Error in 'bash ubuntu19-lxqt.sh'.\n";
+    return 5;
+  }
+  std::cout << "Successfully ran 'bash ubuntu19-lxqt.sh'\n" << SPACER << "\n";
+}
